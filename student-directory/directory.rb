fe3f7a4d794120737c6d08bcc5b576b4dcd126bd
Neil Campbell <neildcampbell@hotmail.com>
2019-08-12 08:42:23 +0100

    add arrays

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,16 +1,16 @@
 student_count = 11
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter",:november],
+  ["Darth Vader",:november],
+  ["Nurse Ratched",:november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
 def print_header
   puts("The students of Villains Academy")
@@ -19,7 +19,7 @@ end
 
 def print_names(students)
   students.each do |student|
-  puts student
+  puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
 
