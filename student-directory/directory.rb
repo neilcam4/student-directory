8b57223c6dc4bf5f2cfebb0a1684986e9849289e
Neil Campbell <neildcampbell@hotmail.com>
2019-08-12 09:19:57 +0100

    add inputs

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,4 +1,16 @@
-student_count = 11
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+     students << {name:name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      name = gets.chomp
+  end
+  students
+end
+
 students = [
   {name:"Dr. Hannibal Lecter",cohort: :november},
   {name:"Darth Vader",cohort: :november},
@@ -26,7 +38,7 @@ end
 def print_footer(students)
   puts "Overall we have #{students.count} great students"
 end
-
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
